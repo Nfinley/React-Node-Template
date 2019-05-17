2a04305407d679af546b08740a963afefb3dd432
Nigel_Finley <nfinley@cfchildren.org>
2019-05-16 19:56:34 -0700

    Added eslint and prettier files and updated server file

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
 // Multi-process to utilize all CPU cores.
+// NOT Relevant if in DEV mode
 if (!isDev && cluster.isMaster) {
   console.error(`Node cluster master ${process.pid} is running`);
 
@@ -16,9 +17,12 @@ if (!isDev && cluster.isMaster) {
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    console.error(
+      `Node cluster worker ${
+        worker.process.pid
+      } exited: code ${code}, signal ${signal}`,
+    );
   });
-
 } else {
   const app = express();
 
@@ -26,17 +30,28 @@ if (!isDev && cluster.isMaster) {
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
   // Answer API requests.
-  app.get('/api', function (req, res) {
+  app.get('/api', (req, res) => {
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.send('{"message":"Hello from the NODE server!"}');
+  });
+
+  app.post('/api', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(req.body));
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  app.get('*', (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, '../react-ui/build', 'index.html'),
+    );
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.error(
+      `Node ${
+        isDev ? 'dev server' : `cluster worker ${process.pid}`
+      }: listening on port ${PORT}`,
+    );
   });
 }
